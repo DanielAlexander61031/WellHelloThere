8bb9e55390b1bfdeaec748954503a134241a1590
61031 <dalexander.61031@farnborough.ac.uk>
2017-09-14 11:42:16 +0100

    I changed it because; why not?

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("How's it going buddy?");
+        System.out.println("I'm a lovely person!");
     }
 }
